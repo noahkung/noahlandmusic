a095f767da471503cc796691494682c7c1fc2b01
noahkung <98510468+noahkung@users.noreply.github.com>
2025-06-07 22:14:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -18,10 +18,10 @@ module.exports = {
   nodes: [
      {
       name: "noahland",
-      password: "nextgencoderspvt",
-      host: "lavalink.nextgencoders.xyz",
-      port:  443,
-      secure: true
+      password: "jmlitelavalink",
+      host: "46.202.82.164 ",
+      port:  1027,
+      secure: false
     }
   ]
 }
